0ebd763638188e499d5f25780e3f15476ea3bd9e
ghostwires <creeperfromdiscord@gmail.com>
2022-10-22 18:08:09 +0800

    Added RQG009

--- edit_transcript.py
+++ edit_transcript.py
@@ -4,17 +4,17 @@ fin.close()
 new_text = ""
 text = text.split('\n')
 for line in text:
+    if line.isdigit() or line == '' or line.isspace():
+        continue
+    if line in ('Rusty Quill Gaming – Episode 9 – Other London'):
+        continue
     line = line.replace('’', '\'')
     line = line.replace('‘', '\'')
     line = line.replace('–', '-')
     line = line.replace(' -', ' --')
-    if line.isdigit() or line == '' or line.isspace():
-        continue
-    if line in ('Trice Forgotten', 'SEAS 5', 'Lay Day'):
-        continue
     if line[0].isdigit():
         line = line[3:]
-    if line[:3] == line[:3].upper():
+    if line[:4] == line[:4].upper():
         line = '#### ' + line.upper()
     new_text += line + '\n'
 fout = open('etr_middle.txt', 'wt')
