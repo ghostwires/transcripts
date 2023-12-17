d319317529dda10f44ff30b5ef318b810a745b84
ghostwires <creeperfromdiscord@gmail.com>
2023-12-17 13:33:40 +0800

    Edited Magnus Archives transcripts to bring up to date with snarp's site; also some other things that I don't remember, probably. It's been a while...

--- edit_transcript.py
+++ edit_transcript.py
@@ -9,7 +9,7 @@ for line in text:
         #gets rid of blank spaces or page numbers
         #it does sometimes accidentally eat the numbers that are results of dice rolls, however. if you want to avoid that you can delete the "line.isdigit()" at the beginning so it doesn't eat any numbers
         continue
-    if line in ('Rusty Quill Gaming – 14 – Knock Knock…'):
+    if line in ('©Rusty Quill 2023', 'Private & Confidential'):
         #put any recurring lines you notice up here. the episode title usually is one, since it occurs in the header of each page on the PDF
         continue
     # if line[:4] == 'Page' and line[-5:] == 'of 19':
@@ -24,7 +24,7 @@ for line in text:
     #     line = line[3:]
     if line[0] == '[':
         line = '##### ' + line
-    elif line == line.upper():
+    elif line.strip("(cont'd)") == line.upper():
         line = '#### ' + line.upper()
     # if line[-1] == ':':
     #     line = '#### ' + line[:-1].upper()
