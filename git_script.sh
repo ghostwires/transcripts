b44bcf7d4ea519b520e29d85aa28f9e8ea3a23d7
ghostwires <creeperfromdiscord@gmail.com>
2024-04-18 07:57:51 -0700

    Added MAGP12

--- git_script.sh
+++ git_script.sh
@@ -1,6 +1,6 @@
 w#!/bin/zsh
 
-REPO_PATH="/Users/home/projects/transcripts"
+REPO_PATH="~/projects/transcripts"
 git -C "$REPO_PATH" add -A
 git -C "$REPO_PATH" commit -m "[the weekly auto-save]"
 git -C "$REPO_PATH" push
