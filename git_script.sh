c7f86a802aa8f2b77fbf0f5a0dcfac7d6f344832
ghostwires <creeperfromdiscord@gmail.com>
2024-04-25 03:22:00 -0700

    [the weekly auto-save]

--- git_script.sh
+++ git_script.sh
@@ -3,5 +3,5 @@
 eval `ssh-agent -s` && ssh-add ~/.ssh/id_ed25519_creeperfromdiscord && ssh-add -l
 REPO_PATH="/Users/home/projects/transcripts/"
 git -C "$REPO_PATH" add -A
-git -C "$REPO_PATH" commit -m "[the weekly auto-save]"
+git -C "$REPO_PATH" commit --allow-empty -m "[the weekly auto-save]"
 git -C "$REPO_PATH" push
